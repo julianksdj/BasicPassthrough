06c57574da50f450c09b6f1b2b2cb1b8f88166af
juliksdj <julian.ksdj@gmail.com>
2023-04-22 18:56:30 +0200

    included lib fftw3

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -8,6 +8,7 @@
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include <fftw3.h>
 
 //==============================================================================
 FftPassthroughAudioProcessor::FftPassthroughAudioProcessor()
